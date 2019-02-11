bf28b8918c58b825f16334b21f94ffbed85e60e6
Max Bos <maxn.bos@gmail.com>
2019-02-11 12:47:16 +0100

    Create main.cpp

---
+++ Project 1 (SAT)/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ * Knowledge Representation: Project 1 (SAT)
+ *
+ * Studentnames:     Max Bos & Erik Stammes
+ * Studentnumbers:   10669027 & ...
+ */
+
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
