ec0ef672c5aaf2a7356dca9dfc792c4254ad35a7
Max Bos <maxn.bos@gmail.com>
2019-02-16 18:18:18 +0100

    Implemented correct unitPropagate and simplify step

--- Project 1 (SAT)/main.cpp
+++ Project 1 (SAT)/main.cpp
@@ -23,13 +23,14 @@ class DavisPutnam {
   string strategy;
   string inputFilePath;
   vector<vector<int> > clauses;
-  tuple<vector<vector<int> >, vector<int>> setup                    (vector<vector<int> >);
+  vector<vector<int> > setup                                        (vector<vector<int> >);
   vector<int> recursive                                             (vector<vector<int> >, vector<int>);
   tuple<vector<vector<int> >, vector<int>> unitPropagate            (vector<vector<int> >, vector<int>);
+  vector<vector<int> > simplify                                     (vector<vector<int> >, int);
   vector<vector<int> > removeTautologies                            (vector<vector<int> >);
   vector<vector<int> > removeItemsByIndices                         (vector<vector<int> >, vector<int>);
-  int getNextLiteral                                                (vector<int>);
-  vector<int> getLiterals                                           (vector<int>);
+  int getNextLiteral                                                (vector<vector<int> >, vector<int>);
+  vector<int> getVariables                                          (vector<int>);
   bool containsEmptyClause                                          (vector<vector<int> >);
 
 public:
@@ -92,56 +93,94 @@ DavisPutnam::DavisPutnam(string strategy, vector<vector<int> > clauses)
   // Initialize the recursive Davis Putnam algorithm with an empty set
   // of assignments.
   vector<int> assignments;
-  tie(clauses, assignments) = setup(clauses);
+  clauses = setup(clauses);
   vector<int> finalAssignments = recursive(clauses, assignments);
+  cout << "The final assignment is:";
   for (auto const& i: finalAssignments) {
     cout << i << " ";
   }
+  cout << endl;
 }
 
 // Perform essential steps before starting the recursive Davis Putnam algorithm,
-// such as removing tautologies from the initial Formula, and assigning the
-// first literal.
-tuple<vector<vector<int> >, vector<int>> DavisPutnam::setup(vector<vector<int> > F) {
+// such as removing tautologies from the initial Formula.
+vector<vector<int> > DavisPutnam::setup(vector<vector<int> > F) {
   F = removeTautologies(F);
-  vector<int> assignments = { getNextLiteral(F) };
-  return make_tuple(F, assignments);
+  // TODO: set all pure literals to true in our assignment.
+  return F;
 }
 
 vector<int> DavisPutnam::recursive(vector<vector<int> > clauses, vector<int> assignments) {
   cout << " recursive call " << endl;
-  tie(clauses, assignments) = unitPropagate(clauses, assignments);
-  cout << "number of clauses left: " << clauses.size() << endl;
+  vector<vector<int> > F;
+  tie(F, assignments) = unitPropagate(clauses, assignments);
+  cout << "number of clauses left: " << F.size() << endl;
   // When the set of clauses contains an empty clause, the problem is unsatisfiable.
-  if (containsEmptyClause(clauses)) return {};
+  if (containsEmptyClause(F)) return {};
   // We have found a successfull assignment when we have no clauses left.
-  if (clauses.empty()) return assignments;
+  if (F.empty()) return assignments;
   // We perform the branching step by picking a literal that is not yet included
-  // in out partial assignment.
-  int literal = getNextLiteral(getLiterals(assignments));
+  // in our partial assignment.
+  int literal = getNextLiteral(F, getVariables(assignments));
+  // Split into the TRUE value for the new variable.
+  F.push_back({ literal });
   assignments.push_back(literal);
-  if (!recursive(clauses, assignments).empty()) return assignments;
-  // Re-set the last assignment to its counterpart value, the False assignment.
-  int lastAssignmentIndex = assignments.size()-1;
-  assignments[lastAssignmentIndex] = assignments[lastAssignmentIndex] * -1;
-  // Call the recursive method with the literal having assigned a False value.
-  return recursive(clauses, assignments);
+  if (!recursive(F, assignments).empty()) return assignments;
+  // If the TRUE value branching step did not yield a successfull assignment,
+  // we try the FALSE value for the same variable.
+  F[F.size()-1] = { -literal };
+  assignments[assignments.size()-1] = -literal;
+  return recursive(F, assignments);
 }
 
+// For each unit clause in the Formula, set the literal from that clause to TRUE,
+// and simplify the Formula.
 tuple<vector<vector<int> >, vector<int>> DavisPutnam::unitPropagate(
   vector<vector<int> > F, vector<int> assignments
 ) {
-  // Save indices of clauses that can be removed and add those variables
-  // to the partial assignments list
-  vector<int> removeIndices;
-  for (int i = 0; i < F.size(); i++) {
-    if (F[i].size() == 1) {
-      removeIndices.push_back(i);
-      assignments.push_back(F[i][0]);
+  vector<vector<int> > newF;
+  for (auto const& clause : F) {
+    if (clause.size() == 1) {
+      int const literal = clause[0];
+      // Simplify the Formula by removing all clauses containing the `literal`
+      // and by removing the literal from a clause where it is `-literal`.
+      newF = simplify(newF, literal);
+      assignments.push_back(literal);
     }
   }
-  F = removeItemsByIndices(F, removeIndices);
-  return make_tuple(F, assignments);
+  return make_tuple(newF, assignments);
+}
+
+// Simplify the Formula by removing all clauses containing the `literal`
+// and by removing the literal from a clause where it is `-literal`.
+vector<vector<int> > DavisPutnam::simplify(vector<vector<int> > F, int subjectLiteral) {
+  vector<vector<int> > newF;
+  int numberOfRemovedClauses = 0;
+  for (int i = 0; i < F.size(); i++) {
+    vector<int> const& clause = F[i];
+    int numberOfRemovedLiterals = 0;
+    for (int j = 0; j < clause.size(); j++) {
+      int const& literal = clause[j];
+      // Remove the clause if it contains the `subjectLiteral`, since this means
+      // the clause is TRUE.
+      if (literal == subjectLiteral) {
+        // Since the index `i` is based on the original Formula, erase the clause at
+        // index `i` adjusted by the number of already removed clauses.
+        newF.erase(newF.begin() + i - numberOfRemovedClauses);
+        numberOfRemovedClauses++;
+      // Remove the `literal` from its clause if it contains the opposite of the
+      // `subjectLiteral`, since this means the `literal` is FALSE.
+      } else if (literal == -subjectLiteral) {
+        // Since the index `j` is based on the original Clause, erase the literal
+        // at index `j` adjusted by the number of already removed literals within
+        // this clause.
+        vector<int> *newClause = &newF.at(i - numberOfRemovedClauses);
+        newClause->erase(newClause->begin() + j - numberOfRemovedLiterals);
+        numberOfRemovedLiterals++;
+      }
+    }
+  }
+  return newF;
 }
 
 vector<vector<int> > DavisPutnam::removeTautologies(vector<vector<int> > F) {
@@ -167,23 +206,33 @@ vector<vector<int> > DavisPutnam::removeItemsByIndices(vector<vector<int> > F, v
   return F;
 }
 
-// Based on the set heuristic, pick the next literal to branch into.
-int DavisPutnam::getNextLiteral(vector<int> currentLiterals) {
-  if (strategy == "S2")
-    return 111;
-  else if (strategy == "S3")
-    return 111;
-  return 111;
+// Based on the set heuristic, pick the next (TRUE) literal to branch into.
+// Iterate through the current formula (clauses set), and find the first variable (TRUE literal)
+// that is not yet already included in our current set of variables.
+int DavisPutnam::getNextLiteral(vector<vector<int> > F, vector<int> currentVariables) {
+  int nextLiteral;
+  for (auto const& clause : F) {
+    for (int const& literal : clause) {
+      const bool literalIsAlreadyAssigned = find(
+        currentVariables.begin(), currentVariables.end(), abs(literal)
+      ) != currentVariables.end();
+      if (literal > 0 && !literalIsAlreadyAssigned ) {
+        nextLiteral = abs(literal);
+        break;
+      }
+    }
+  }
+  return nextLiteral;
 }
 
 // Calculates the absolute (TRUE) values of each assignment, which represents
 // each literal.
-vector<int> DavisPutnam::getLiterals(vector<int> assignments) {
-  vector<int> literals;
+vector<int> DavisPutnam::getVariables(vector<int> assignments) {
+  vector<int> variables;
   for (auto const& el : assignments) {
-    literals.push_back(abs(el));
+    variables.push_back(abs(el));
   }
-  return literals;
+  return variables;
 }
 
 // Checks whether a given set of clauses contains an empty clause.
